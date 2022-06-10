5d52e902f9dbf561b78106fef5a80dd922c08cb6
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2022-06-09 23:09:44 -0300

    Blocos try catch

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -6,9 +6,6 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            // ContaCorrente conta = new ContaCorrente(7480, 874250);
-            // Console.WriteLine(ContaCorrente.TaxaOperacao);
-
             Metodo();
 
             Console.ReadLine();
@@ -16,43 +13,26 @@ namespace ByteBank
 
         private static void Metodo()
         {
-            TestaDivisao(0);
-            TestaDivisao(20);
+            try
+            {
+                TestaDivisao(0);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Ocorreu um erro! Não é possível dividir um número por 0!");
+            }
         }
 
         private static void TestaDivisao(int divisor)
         {
             int resultado = Dividir(10, divisor);
 
-            if (resultado == -1)
-            {
-                Console.WriteLine("Não é possível fazer divisão por 0.");
-            } else
-            {
-                Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado + ".");
-            }
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado + ".");
         }
 
         private static int Dividir(int numero, int divisor)
         {
-            if (divisor == 0)
-            {
-                return -1;
-            }
-
-            if (divisor > numero)
-            {
-                return -2;
-            }
-
             return numero / divisor;
         }
-
-        /*
-         * Dividir
-         * TestaDivisao
-         * Metodo
-         * Main
-        */
     }
 }
